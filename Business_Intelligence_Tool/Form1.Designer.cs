f81e2e10c692e4574b6727b4d349a8726f15a2f7
livcoll21 <82226998+livcoll21@users.noreply.github.com>
2021-04-21 21:50:58 +0100

    Add files via upload

---
+++ Business_Intelligence_Tool/Form1.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace Business_Intelligence_Tool
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series5 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series6 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.chart2 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.btnRead = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            chartArea1.AxisX.IsLabelAutoFit = false;
+            chartArea1.AxisX.LabelStyle.Angle = -90;
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(12, 12);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.LegendText = "Yearly Totals";
+            series1.Name = "Series1";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(373, 330);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            this.chart1.Click += new System.EventHandler(this.chart1_Click);
+            // 
+            // chart2
+            // 
+            this.chart2.BackColor = System.Drawing.Color.LightGray;
+            chartArea2.AxisX.IsLabelAutoFit = false;
+            chartArea2.AxisX.LabelStyle.Angle = -90;
+            chartArea2.Name = "ChartArea1";
+            this.chart2.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.chart2.Legends.Add(legend2);
+            this.chart2.Location = new System.Drawing.Point(391, 12);
+            this.chart2.Name = "chart2";
+            series2.BorderWidth = 3;
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series2.Legend = "Legend1";
+            series2.LegendText = "Funky Chicken";
+            series2.Name = "Funky Chicken";
+            series3.BorderWidth = 3;
+            series3.ChartArea = "ChartArea1";
+            series3.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series3.Legend = "Legend1";
+            series3.LegendText = "Barry\'s Burgers";
+            series3.Name = "Barry\'s Burgers";
+            series4.BorderWidth = 3;
+            series4.ChartArea = "ChartArea1";
+            series4.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series4.LabelBorderWidth = 3;
+            series4.Legend = "Legend1";
+            series4.LegendText = "Izzys Ice Cream";
+            series4.Name = "Izzys Ice Cream";
+            series5.BorderWidth = 3;
+            series5.ChartArea = "ChartArea1";
+            series5.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series5.Legend = "Legend1";
+            series5.LegendText = "Pizza Petes";
+            series5.Name = "Pizza Petes";
+            series6.BorderWidth = 3;
+            series6.ChartArea = "ChartArea1";
+            series6.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series6.Legend = "Legend1";
+            series6.LegendText = "Kevs Kebabs";
+            series6.Name = "Kevs Kebabs";
+            this.chart2.Series.Add(series2);
+            this.chart2.Series.Add(series3);
+            this.chart2.Series.Add(series4);
+            this.chart2.Series.Add(series5);
+            this.chart2.Series.Add(series6);
+            this.chart2.Size = new System.Drawing.Size(662, 300);
+            this.chart2.TabIndex = 1;
+            this.chart2.Text = "chart2";
+            // 
+            // btnRead
+            // 
+            this.btnRead.Location = new System.Drawing.Point(634, 324);
+            this.btnRead.Name = "btnRead";
+            this.btnRead.Size = new System.Drawing.Size(127, 23);
+            this.btnRead.TabIndex = 2;
+            this.btnRead.Text = "Display Data";
+            this.btnRead.UseVisualStyleBackColor = true;
+            this.btnRead.Click += new System.EventHandler(this.btnRead_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(388, 329);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Enter Year";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(451, 326);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(177, 20);
+            this.textBox1.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1065, 355);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnRead);
+            this.Controls.Add(this.chart2);
+            this.Controls.Add(this.chart1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart2;
+        private System.Windows.Forms.Button btnRead;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Business_Intelligence_Tool/Form1.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace Business_Intelligence_Tool
+{
+    public partial class Form1 : Form
+    {
+        ///////////////////////////////////////////////////////////////////////////
+
+        // GLOBALS
+
+        // Global data for storing data and for plotting lines
+        int year;
+        string[] fileData = new string[25];
+        double[] plotLines = new double[12];
+        double[] plotLinesFC = new double[12]; // data for Funky Chicken
+        double[] plotLinesBB = new double[12]; // data for Barry's Burgers
+        double[] plotLinesII = new double[12]; // data for Izzys Ice Cream
+        double[] plotLinesPP = new double[12]; // data for Pizza Petes
+        double[] plotLinesKK = new double[12]; // data for Kevs Kebabs
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        ///////////////////////////////////////////////////////////////////////////
+
+        // BUTTON READ FILE, BUILD DATA, PRESENT DATA (All in one)
+
+        private void btnRead_Click(object sender, EventArgs e)
+        {
+            // Local path to bin file
+
+            string fileName = "mySalesData.csv";
+
+            System.IO.StreamReader objReader;
+            objReader = new System.IO.StreamReader(fileName);
+
+            // Reads all data from file into internal array
+
+            int len = 25;
+
+            for (int i = 0; i < len; i++)
+            {
+                fileData[i] = objReader.ReadLine();
+            }
+
+            objReader.Close();
+
+            ///////////////////////////////////////////////////////////////////////////
+
+            // LINE CHART  
+
+            string salesData;
+            string[] plotData;
+
+            if (textBox1.Text.Length != 0)
+            {
+
+                year = int.Parse(textBox1.Text);
+
+                for (int i = 0; i < 25; i++) // step through whole array looking for years
+                {
+                    salesData = fileData[i];
+                    plotData = salesData.Split(',');
+                    if (int.Parse(plotData[1]) == (year))
+                    {
+
+                        // build data for Funky Chicken
+                        if (plotData[0] == "Funky Chicken")
+                        {
+                            for (int j = 0; j < 12; j++)
+                            {
+                                plotLinesFC[j] = double.Parse(plotData[j + 2]);
+                            }
+                        }
+
+                        // build data for Barry's Burgers
+                        if (plotData[0] == "Barrys Burgers")
+                        {
+                            for (int j = 0; j < 12; j++)
+                            {
+                                plotLinesBB[j] = double.Parse(plotData[j + 2]);
+                            }
+                        }
+
+                        // build data for Izzys Ice Cream
+                        if (plotData[0] == "Izzys Ice cream")
+                        {
+                            for (int j = 0; j < 12; j++)
+                            {
+                                plotLinesII[j] = double.Parse(plotData[j + 2]);
+                            }
+                        }
+
+                        // build data for Pizza Petes
+                        if (plotData[0] == "Pizza Petes")
+                        {
+                            for (int j = 0; j < 12; j++)
+                            {
+                                plotLinesPP[j] = double.Parse(plotData[j + 2]);
+                            }
+                        }
+
+                        // build data for Kevs Kababs
+                        if (plotData[0] == "Kevs Kebabs")
+                        {
+                            for (int j = 0; j < 12; j++)
+                            {
+                                plotLinesKK[j] = double.Parse(plotData[j + 2]);
+                            }
+                        }
+                    }
+                    
+                }
+
+                // adds some chart labels for months
+
+                string[] months = new string[12] { "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec" };
+
+                CustomLabel customlabel;
+
+                Axis axisX = chart2.ChartAreas[0].AxisX;
+                double axelLabelPos = 0.5;
+                for (int i = 0; i < 12; i++)
+                {
+                    customlabel = axisX.CustomLabels.Add(axelLabelPos, axelLabelPos + 1, months[i]);
+                    axelLabelPos++;
+                }
+
+                chart2.Series["Funky Chicken"].Points.DataBindY(plotLinesFC);
+                chart2.Series["Barry's Burgers"].Points.DataBindY(plotLinesBB);
+                chart2.Series["Izzys Ice Cream"].Points.DataBindY(plotLinesII);
+                chart2.Series["Pizza Petes"].Points.DataBindY(plotLinesPP);
+                chart2.Series["Kevs Kebabs"].Points.DataBindY(plotLinesKK);
+            }
+
+            ///////////////////////////////////////////////////////////////////////////
+
+            // BAR CHART
+
+
+            // sets the titles and year totals array
+            string[] titles = new string[5] { "Funky Chicken", "Barrys Burgers", "Izzys Ice Cream", "Pizza Pete", "Kevs Kebabs" };
+            double[] yearTotals = new double[5];
+
+            CustomLabel customlabel1;
+            Axis barChartAxisX = chart1.ChartAreas[0].AxisX;
+
+            double axelLabelPos1 = 0.5;
+
+            for (int i = 0; i < 5; i++)
+            {
+                customlabel1 = barChartAxisX.CustomLabels.Add(axelLabelPos1, axelLabelPos1 + 1, titles[i]);
+                axelLabelPos1++;
+            }
+
+            // calculate year yearTotals here
+
+            for (int j = 0; j < 12; j++)
+            {
+                yearTotals[0] = yearTotals[0] + plotLinesFC[j];
+                yearTotals[1] = yearTotals[1] + plotLinesBB[j];
+                yearTotals[2] = yearTotals[2] + plotLinesII[j];
+                yearTotals[3] = yearTotals[3] + plotLinesPP[j];
+                yearTotals[4] = yearTotals[4] + plotLinesKK[j];
+            }
+            chart1.Series["Series1"].Points.DataBindY(yearTotals);
+        }
+
+        private void chart1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
